f2fbe356242dac37b7df5866130bc99adcaeef4c
super <112580881+ksalx@users.noreply.github.com>
2023-09-13 00:58:29 +0300

    yES

--- test.py
+++ test.py
@@ -6,26 +6,50 @@ from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 from telethon import events, TelegramClient
 
 load_dotenv()
 
-api_id = os.getenv('api_id')
+api_id = int(os.getenv('api_id'))
 api_hash = os.getenv('api_hash')
 client = TelegramClient('anon', api_id, api_hash)
 
-all_codes = []
-last_codes = []
+
+@client.on(events.NewMessage(pattern='^[A-Z0-9]{8}$'))
+async def message_handler(event):
+    text = event.message.text
+    check_handle_message(text)
 
 
-def simulate_browser(driver, code: str):
+def show_result(data):
+    print(data['code'], data['amount'], data['coin'])
+
+
+def check_box(code):
+    try:
+        btn = WebDriverWait(driver, timeout=3).until(
+            ec.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[1]/div/div/button'))
+        )
+        open_btn = driver.find_element(By.XPATH, value='/html/body/div[4]/div[1]/div/div/button').click()
+        time.sleep(3)
+        amount = driver.find_element(By.XPATH, value='/html/body/div[4]/div[1]/div/div/div[4]/div/div/div[1]').text
+        coin = driver.find_element(By.XPATH, value='/html/body/div[4]/div[1]/div/div/div[4]/div/div/div[2]').text
+        print(code, amount, coin)
+        driver.refresh()
+        driver.implicitly_wait(3)
+    except:
+        print('Криптобокс закончился или уже использован')
+        driver.refresh()
+        driver.implicitly_wait(3)
+
+
+def open_box(code: str):
     all_codes.append(code)
-    print('All Codes', all_codes)
+    print(last_codes)
     if len(last_codes) == 1000:
         last_codes.clear()
-    print(last_codes)
-    print(code)
-    print(driver.get_window_size())
     if code in last_codes:
         print(f'skip: {code}')
     else:
@@ -38,49 +62,19 @@ def simulate_browser(driver, code: str):
                                            value='/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[2]'
                                                  '/button').click())
 
-        time.sleep(1.8)
-        try:
-            open_box_btn = driver.find_element(by=By.XPATH, value='/html/body/div[8]/div[1]/div/div/button/div').text
-            if open_box_btn == 'Открыть':
-                open_box_btn = driver.find_element(by=By.XPATH,
-                                                   value='/html/body/div[8]/div[1]/div/div/button/div').click()
+        last_codes.append(code)
 
-                driver.implicitly_wait(3)
-                open_box_btn.click()
-                time.sleep(3)
-                balance = driver.find_element(by=By.XPATH,
-                                              value='/html/body/div[8]/div/div[3]/div[1]/div[2]/div[2]').text
-                print(code, balance)
-                last_codes.append(code)
-                driver.implicitly_wait(3)
-                driver.refresh()
-                driver.implicitly_wait(3)
-            else:
-                print('Бокс пустой')
-                last_codes.append(code)
-                driver.refresh()
-                driver.implicitly_wait(3)
-        except Exception as E:
-            print('Закончились')
-            last_codes.append(code)
-            driver.refresh()
-            driver.implicitly_wait(3)
+        check_box(code)
 
 
-def check_handle_message(message: str, driver) -> None:
+def check_handle_message(message: str) -> None:
     if len(message) > 8:
         message = message.replace('*', '').replace('`', '')
-    simulate_browser(driver, message)
+    open_box(message)
 
 
-@client.on(events.NewMessage(pattern='^[A-Z0-9]{8}$'))
-async def message_handler(event):
-    text = event.message.text
-    check_handle_message(text, driver)
-
-
-def start_browser(profile_id: str):
-    url = f'http://localhost:3001/v1.0/browser_profiles/{profile_id}/start?automation=1'
+def start_browser(value: str):
+    url = f'http://localhost:3001/v1.0/browser_profiles/{value}/start?automation=1'
     response = requests.get(url)
     print(response.status_code)
     port = response.json()['automation']['port']
@@ -90,9 +84,9 @@ def start_browser(profile_id: str):
     options.debugger_address = f'127.0.0.1:{port}'
     # options.add_argument('--headless')
 
-    driver = webdriver.Chrome(service=chromedriver_path, options=options)
-    driver.set_window_position(x=1099, y=0)
-    return driver
+    browser = webdriver.Chrome(service=chromedriver_path, options=options)
+    # driver.set_window_position(x=1099, y=0)
+    return browser
 
 
 async def main():
@@ -101,7 +95,9 @@ async def main():
 
 
 if __name__ == "__main__":
-    profile_id = '149638751'
+    all_codes = []
+    last_codes = []
+    profile_id = os.getenv('pofile_id')
     driver = start_browser(profile_id)
     with client:
         client.loop.run_until_complete(main())
